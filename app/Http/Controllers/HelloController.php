4e5a1302743a32ebefb192f7ae8ea8bec8dd1c37
Your Name <levanmanha2@gmail.com>
2019-07-16 16:12:11 +0700

    lab2

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function hello()
+    {
+        $name = 'Manh';
+    	$tuoi = 21;
+    	$diachi = 'Ha Noi';
+    	$lop = ' PT1111';
+    	return view('hello',
+    	 [
+    	 	'ten'=> $name,
+    	 	'b' => $tuoi,
+    	 	'c' => $diachi,
+    	 	'd' =>$lop,
+    	 ]);
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/hello.blade.php
@@ -0,0 +1,31 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Tieu De</th>
+      <th scope="col">Noi Dung</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Tên</th>
+      <td>{{$ten}}</td>
+    
+    </tr>
+    <tr>
+      <th scope="row">Tuổi</th>
+      <td>{{$b}}</td>
+     
+    </tr>
+<tr>
+      <th scope="row">Địa chỉ</th>
+      <td>{{$c}}</td>
+  </tr>
+  <tr>
+      <th scope="row">Lớp</th>
+      <td>{{$d}}</td>
+  </tr>
+</tbody>
+</table>
